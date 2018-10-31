a8e42a5453f6434c05279612ff74c07d553414e9
Cookie1981 <david.cook@bglgroup.co.uk>
2018-10-31 16:41:02 +0000

    Upload Sample Project

---
+++ src/Tool/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Days2GoGo
+{
+    using McMaster.Extensions.CommandLineUtils;
+    
+    [Subcommand("Hello", typeof(HelloCommand))]
+    [Subcommand("Until", typeof(UntilCommand))]
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var app = new CommandLineApplication<Program>();
+            app.Conventions.UseDefaultConventions();
+
+            try
+            {
+                return app.Execute(args);
+            }
+            catch (CommandParsingException cpe)
+            {
+                Console.WriteLine(cpe.Message);
+                return -1;
+            }
+            finally
+            {
+#if DEBUG
+                Console.ReadLine();
+#endif
+            }
+        }
+    }
+}
---
+++ src/Tool/UntilCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Days2GoGo
+{
+    using McMaster.Extensions.CommandLineUtils;
+    
+    [Command(Description = "Get a count down to a big date you are waiting for")]
+    public class UntilCommand
+    {
+        [Argument(1, Name = "EventName", Description = "Name of the event you wish to count down to.")]
+        [Required]
+        public string EventName { get; } = "your big day";
+
+        [Option(CommandOptionType.SingleValue, Description = "The date you are counting down to.")]
+        public string Date { get; }
+
+        [Option(CommandOptionType.MultipleValue, Description = "Any messages that relate to the event.")]
+        public string[] Messages { get; }
+
+        public int OnExecute(CommandLineApplication app, IConsole console)
+        {
+            if (!DateTime.TryParse(Date, out var eventDate))
+            {
+                console.WriteLine("Invalid date.");
+                return 0;
+            }
+
+            var today = DateTime.Today;
+
+            var totalDays = (eventDate - today).TotalDays;
+
+            console.WriteLine($"Only {totalDays} days to go before {EventName}.");
+
+            foreach (var message in Messages)
+            {
+                console.WriteLine(message);
+            }
+
+            return 0;
+        }
+    }
+
+    public class HelloCommand
+    {
+        public int OnExecute(CommandLineApplication app, IConsole console)
+        {
+            console.WriteLine("Hello World!");
+
+            return 0;
+        }
+    }
+}
